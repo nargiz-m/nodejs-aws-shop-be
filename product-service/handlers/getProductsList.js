6470070cc62af0b4713734febb278663f4f604ea
Nargiz Mamedova <nargizm7@yandex.ru>
2023-11-20 02:03:24 +0300

    [T-3] mock file modified, cors error fixed

--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -3,6 +3,7 @@ const { products } = require('./mock/mocks')
 exports.handler = async () => {
     return {
       statusCode: 200,
+      headers: { "Access-Control-Allow-Origin": "*" },
       body: JSON.stringify(products)
     };
 };
--- product-service/handlers/mock/mocks.js
+++ product-service/handlers/mock/mocks.js
@@ -3,13 +3,13 @@ exports.products = [
     description: "Short Product Description1",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
     price: 24,
-    title: "ProductOne",
+    title: "AWSProductOne",
   },
   {
     description: "Short Product Description7",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
     price: 15,
-    title: "ProductTitle",
+    title: "AWSProductTitle",
   },
   {
     description: "Short Product Description2",
