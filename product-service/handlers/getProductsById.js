a4f22eff2b56e9db4dec29319d6c0f8efe6b7442
Nargiz Mamedova <nargizm7@yandex.ru>
2023-11-20 00:58:45 +0300

    [T-3] mock file added

--- product-service/handlers/getProductsById.js
+++ product-service/handlers/getProductsById.js
@@ -1,6 +1,17 @@
+const { products } = require("./mock/mocks");
+
 exports.handler = async (event) => {
+  const product = products.find(product => product.id === event.pathParameters.product_id)
+   
+  if (product) {
     return {
       statusCode: 200,
-      body: JSON.stringify(`Hello, CDK! You've hit ${event.path}\n`)
+      body: JSON.stringify(product)
     };
+  }
+
+  return {
+    statusCode: 404,
+    body: 'Product not found'
+  }
 };
--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -1,11 +1,8 @@
+const { products } = require('./mock/mocks')
+
 exports.handler = async () => {
     return {
       statusCode: 200,
-      body: JSON.stringify([{
-        id: 1,
-        title: 'Book',
-        description: 'A great book',
-        price: 2
-      }])
+      body: JSON.stringify(products)
     };
 };
