dac271d09f4af4efccfbc0c52acd1f0b63ad522d
Nargiz Mamedova <nargizm7@yandex.ru>
2023-11-27 19:52:25 +0300

    [T-4] added transactions

--- product-service/ddb-table-populate.js
+++ product-service/ddb-table-populate.js
@@ -1,7 +1,7 @@
 import products from './mocks.js';
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { fromIni } from "@aws-sdk/credential-providers";
+import { DynamoDBDocumentClient, TransactWriteCommand } from '@aws-sdk/lib-dynamodb';
 
 const dbClient = new DynamoDBClient({
   credentials: fromIni({profile: 'default'}),
@@ -9,25 +9,27 @@ const dbClient = new DynamoDBClient({
 const docClient = DynamoDBDocumentClient.from(dbClient);
 
 products.forEach(async (product) => {
-    const command = new PutCommand({
-        TableName: "Products",
-        Item: {
-            id: product.id,
-            title: product.title,
-            description: product.description,
-            price: product.price,
-        }
+    const input = new TransactWriteCommand({
+        TransactItems: [{
+            Put: {
+                Item: {
+                    id: product.id,
+                    title: product.title,
+                    description: product.description,
+                    price: product.price,                
+                },
+                TableName: "Products",
+            },
+            Put: {
+                Item: {
+                    product_id: product.id,
+                    count: Math.floor(Math.random() * 25)
+                },
+                TableName: "Stocks",
+            },
+          },
+        ]
     });
-    const stockCommand = new PutCommand({
-        TableName: "Stocks",
-        Item: {
-            product_id: product.id,
-            count: 10
-        }
-    });
-      
-    const response = await docClient.send(command);
-    const stockResponse = await docClient.send(stockCommand);
+    const response = await docClient.send(input);
     console.log(JSON.stringify(response))
-    console.log(JSON.stringify(stockResponse))
 })
