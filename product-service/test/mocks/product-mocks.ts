c401ed338373cf7df6e939df66a20f357e0f214f
Nargiz Mamedova <nargizm7@yandex.ru>
2023-11-28 14:22:36 +0300

    [T-4] fixed broken test

---
+++ product-service/test/mocks/product-mocks.ts
@@ -0,0 +1,44 @@
+export default [
+    {
+      description: "Short Product Description1",
+      id: "7567ec4b-b10c-49c5-9345-fc73c48a80aa",
+      price: 24,
+      count: 24,
+      title: "Different AWS Product",
+    },
+    {
+      description: "Short Product Description7",
+      id: "7567ec4b-b10c-49c5-9345-fc73c48a80a1",
+      price: 15,
+      count: 15,
+      title: "AWS Product",
+    },
+    {
+      description: "Short Product Description2",
+      id: "7567ec4b-b10c-49c5-9345-fc73c48a80a3",
+      price: 23,
+      count: 23,
+      title: "Product Product",
+    },
+    {
+      description: "Short Product Description4",
+      id: "7567ec4b-b10c-49c5-9345-fc73348a80a1",
+      price: 15,
+      count: 15,
+      title: "Test Product",
+    },
+    {
+      description: "Short Product Descriptio1",
+      id: "7567ec4b-b10c-49c5-9445-fc73c48a80a2",
+      price: 23,
+      count: 0,
+      title: "Expensive Product",
+    },
+    {
+      description: "Short Product Description7",
+      id: "7567ec4b-b10c-49c5-9345-fc73c48a80a1",
+      price: 15,
+      count: 15,
+      title: "Normal Product",
+    },
+]
--- product-service/test/product-service.test.ts
+++ product-service/test/product-service.test.ts
@@ -1,21 +1,27 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { handler as getProductsById } from "../handlers/getProductsById";
 import { handler as getProductsList } from "../handlers/getProductsList";
+import { getProductsWithStocks } from "../services/getProductsWithStocks";
+import { getSingleProduct } from "../services/getSingleProduct";
+import productMocks from "./mocks/product-mocks";
+
+jest.mock('../services/getProductsWithStocks')
+jest.mock('../services/getSingleProduct')
 
 describe('Product Service unit tests', () => {
   test("getProductsList returns list of products", async () => {
+    jest.mocked(getProductsWithStocks).mockImplementation(() => Promise.resolve([...productMocks]))
     const productsResponse = await getProductsList();
 
     expect(productsResponse.statusCode).toEqual(200);
     expect(JSON.parse(productsResponse.body).length).toBeGreaterThan(0);
   }) 
-
   
   test("getProductsById returns a product information when sending correct id", async () => {
-    const productsResponse = await getProductsList();
+    jest.mocked(getSingleProduct).mockImplementation(() => Promise.resolve(productMocks[0]))
     const fakeEvent: Partial<APIGatewayProxyEvent> = {
       pathParameters: {
-        product_id: JSON.parse(productsResponse.body)[0].id
+        product_id: productMocks[0].id
       }
     }
     const productResponse = await getProductsById(fakeEvent as APIGatewayProxyEvent);
@@ -27,6 +33,7 @@ describe('Product Service unit tests', () => {
   }) 
 
   test("getProductsById returns an error page when sending incorrect id", async () => {
+    jest.mocked(getSingleProduct).mockImplementation(() => Promise.resolve(undefined))
     const fakeEvent: Partial<APIGatewayProxyEvent> = {
       pathParameters: {
         product_id: 'random'
