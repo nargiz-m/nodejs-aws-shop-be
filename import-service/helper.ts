2ca814b9d3a336c4923a992eaff2d17b9218e7da
Nargiz Mamedova <nargizm7@yandex.ru>
2023-12-14 11:17:22 +0300

    [T-6] minor changes

--- import-service/helper.ts
+++ import-service/helper.ts
@@ -1,10 +1,12 @@
 import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+import stripBomStream from "strip-bom-stream";
 import csvParser = require("csv-parser");
 
 export const parseFile = (stream: NodeJS.ReadableStream) => {
     const results: string[] = [];
     return new Promise((resolve, reject) => {
         stream
+        .pipe(stripBomStream())
         .pipe(csvParser())
         .on('data', (data) => results.push(data))
         .on('end', () => {
