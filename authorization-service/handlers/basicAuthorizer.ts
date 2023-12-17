e8b896a1076f2453175f1935342889a059c233ec
Nargiz Mamedova <nargizm7@yandex.ru>
2023-12-18 02:05:49 +0300

    [T-7] added credential check

--- authorization-service/handlers/basicAuthorizer.ts
+++ authorization-service/handlers/basicAuthorizer.ts
@@ -12,7 +12,14 @@ export const handler = (event: APIGatewayProxyEvent) => {
         const creds = atob(token.split(" ")[1]);
         const [username, password] = creds.split(':');
 
-        return;
+        if(! Object.keys(process.env).includes(username) || process.env[username] !== password) {
+            throw new Error();   
+        }
+
+        return {
+            statusCode: 200,
+            body: 'Success'
+        };
     } catch (error) {
         return {
             statusCode: 403,
--- authorization-service/lib/authorization-service.ts
+++ authorization-service/lib/authorization-service.ts
@@ -1,6 +1,7 @@
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import 'dotenv/config';
 
 export class AuthorizationService extends Construct {
     constructor(scope: Construct, id: string) {
