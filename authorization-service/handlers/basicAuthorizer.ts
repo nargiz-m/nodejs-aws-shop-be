d18073e1b58ab20c1663ab8b2e4f372c575a2bdc
Nargiz Mamedova <nargizm7@yandex.ru>
2023-12-19 20:59:05 +0300

    [T-7] decoding updated

--- authorization-service/handlers/basicAuthorizer.ts
+++ authorization-service/handlers/basicAuthorizer.ts
@@ -4,7 +4,7 @@ export const handler = async (event: APIGatewayTokenAuthorizerEvent) => {
     const token = event.authorizationToken; 
     let effect = 'Allow';
     try {
-        const creds = atob(token.split(" ")[1]);
+        const creds = Buffer.from(token.split(" ")[1], 'base64').toString();
         const [username, password] = creds.split(':');
 
         if(!Object.keys(process.env).includes(username) || process.env[username] !== password) {
